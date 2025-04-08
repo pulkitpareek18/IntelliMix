28a630bb26ba62c1237b65e1b08e093f45f152ff
Pulkit Pareek <84959590+pulkitpareek18@users.noreply.github.com>
2025-04-08 18:09:45 +0530

    Add host

--- backend/app.py
+++ backend/app.py
@@ -232,4 +232,4 @@ def audio_download():
         return jsonify({"error": f"Error downloading audio: {str(e)}"}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
