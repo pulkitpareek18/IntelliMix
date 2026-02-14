8cfeb603325d20af985cdd2cf81faf3a5320bdf9
Praveen Sharma <praveen144204@gmail.com>
2026-02-14 21:11:14 +0530

    Finalized Changes

--- backend_auth_db/middlewares/auth_middleware.js
+++
@@ -1,8 +0,0 @@
-const auth_middleware = (req, res, next) => {
-    if (!req.cookies.auth_token || req.cookies.auth_token === 'undefined') {
-        return res.status(401).json({ message: 'Unauthorized' });
-    }
-    next();
-};
-
-export default auth_middleware;
